47088b52e53261928e8f853e36aae0e407cc9f23
Leu <zl4589130@gmail.com>
2015-08-11 22:06:14 +0800

    初始化了toolbar和drawerlayout

---
+++ app/src/main/java/com/leu/littleweather/BaseActivity.java
@@ -0,0 +1,41 @@
+package com.leu.littleweather;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+import com.balysv.materialmenu.MaterialMenuDrawable;
+
+/**
+ * Created by Leu on 2015/8/11.
+ */
+public class BaseActivity extends AppCompatActivity {
+    protected Toolbar mToolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("BaseActivity",getClass().getSimpleName());
+    }
+
+    /**
+     * 根据int得出对应的图标状态
+     * @param state
+     * @return 图标状态，默认为汉堡型
+     */
+    protected MaterialMenuDrawable.IconState intToState(int state){
+        switch (state){
+            case 0:
+                return MaterialMenuDrawable.IconState.BURGER;
+            case 1:
+                return MaterialMenuDrawable.IconState.ARROW;
+            case 2:
+                return MaterialMenuDrawable.IconState.X;
+            case 3:
+                return MaterialMenuDrawable.IconState.CHECK;
+        }
+
+        return MaterialMenuDrawable.IconState.BURGER;
+    }
+}
--- app/src/main/java/com/leu/littleweather/MainActivity.java
+++ app/src/main/java/com/leu/littleweather/MainActivity.java
@@ -1,14 +1,32 @@
 package com.leu.littleweather;
 
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.balysv.materialmenu.MaterialMenuDrawable;
+
+public class MainActivity extends BaseActivity {
+
+    private MaterialMenuDrawable materialMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                // Handle your drawable state here
+
+            }
+        });
+        materialMenu = new MaterialMenuDrawable(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN);
+        toolbar.setNavigationIcon(materialMenu);
+
     }
 
 
