404e5fa29a363d050931173b5b0b579377db1707
Leu <zl4589130@gmail.com>
2015-08-12 22:04:16 +0800

    把抽屉折腾了一下

--- app/src/main/java/com/leu/littleweather/MainActivity.java
+++ app/src/main/java/com/leu/littleweather/MainActivity.java
@@ -2,30 +2,74 @@ package com.leu.littleweather;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 
 import com.balysv.materialmenu.MaterialMenuDrawable;
+import com.balysv.materialmenu.extras.toolbar.MaterialMenuIconToolbar;
 
 public class MainActivity extends BaseActivity {
 
-    private MaterialMenuDrawable materialMenu;
+    private MaterialMenuIconToolbar mMaterialMenu;
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
+        initView();
+
+
+    }
+
+    private void initView() {
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 // Handle your drawable state here
+                if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
+                    mDrawerLayout.closeDrawer(Gravity.LEFT);
+                } else {
+                    mDrawerLayout.openDrawer(Gravity.LEFT);
+                }
+            }
+        });
+        mMaterialMenu = new MaterialMenuIconToolbar(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN) {
+            @Override
+            public int getToolbarViewId() {
+                return R.id.toolbar;
+            }
+        };
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                mMaterialMenu.setTransformationOffset(MaterialMenuDrawable.AnimationState.BURGER_ARROW,
+                        slideOffset);
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                mMaterialMenu.animatePressedState(intToState(1));
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                mMaterialMenu.animatePressedState(intToState(0));
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
 
             }
         });
-        materialMenu = new MaterialMenuDrawable(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN);
-        toolbar.setNavigationIcon(materialMenu);
 
     }
 
