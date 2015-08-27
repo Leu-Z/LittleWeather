05324253484a7bca7233eb8e7ad21d6c207c6129
Leu <zl4589130@gmail.com>
2015-08-27 22:08:41 +0800

    把左上角的导航图标实现方式简化了，用Navigation View实现了抽屉功能

--- app/src/main/java/com/leu/littleweather/BaseActivity.java
+++ app/src/main/java/com/leu/littleweather/BaseActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 
-import com.balysv.materialmenu.MaterialMenuDrawable;
-
 /**
  *
  * Created by Leu on 2015/8/11.
@@ -21,23 +19,4 @@ public class BaseActivity extends AppCompatActivity {
         Log.d("BaseActivity",getClass().getSimpleName());
     }
 
-    /**
-     * 根据int得出对应的图标状态
-     * @param state
-     * @return 图标状态，默认为汉堡型
-     */
-    protected MaterialMenuDrawable.IconState intToState(int state){
-        switch (state){
-            case 0:
-                return MaterialMenuDrawable.IconState.BURGER;
-            case 1:
-                return MaterialMenuDrawable.IconState.ARROW;
-            case 2:
-                return MaterialMenuDrawable.IconState.X;
-            case 3:
-                return MaterialMenuDrawable.IconState.CHECK;
-        }
-
-        return MaterialMenuDrawable.IconState.BURGER;
-    }
 }
--- app/src/main/java/com/leu/littleweather/MainActivity.java
+++ app/src/main/java/com/leu/littleweather/MainActivity.java
@@ -1,22 +1,19 @@
 package com.leu.littleweather;
 
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
-import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Toast;
-
-import com.balysv.materialmenu.MaterialMenuDrawable;
-import com.balysv.materialmenu.extras.toolbar.MaterialMenuIconToolbar;
 
 public class MainActivity extends BaseActivity {
 
-    private MaterialMenuIconToolbar mMaterialMenu;
     private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,80 +28,53 @@ public class MainActivity extends BaseActivity {
     private void initView() {
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
+        //设置菜单回调
         mToolbar.setOnMenuItemClickListener(onMenuItemClick);
-        //按下toolbar导航键会分别打开或关闭抽屉
-        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle your drawable state here
-                if (mDrawerLayout.isDrawerOpen(Gravity.LEFT)) {
-                    mDrawerLayout.closeDrawer(Gravity.LEFT);
-                } else {
-                    mDrawerLayout.openDrawer(Gravity.LEFT);
-                }
-            }
-        });
-        //把MaterialMenuIcon与Toolbar关联
-        mMaterialMenu = new MaterialMenuIconToolbar(this, Color.WHITE, MaterialMenuDrawable.Stroke.THIN) {
-            @Override
-            public int getToolbarViewId() {
-                return R.id.toolbar;
-            }
-        };
-
+        //设置toolbar的左上角图标
+        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
-        //根据抽屉的各种状态或动作，执行相印的MaterialMenuIcon变化
-        mDrawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.drawer_open,
+                R.string.drawer_close);
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
 
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset) {
-                mMaterialMenu.setTransformationOffset(MaterialMenuDrawable.AnimationState.BURGER_ARROW,
-                        slideOffset);
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-                mMaterialMenu.animatePressedState(intToState(1));
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-                mMaterialMenu.animatePressedState(intToState(0));
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
-
-        findViewById(R.id.drawer_item_controlcity).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "点中", Toast.LENGTH_SHORT).show();
-            }
-        });
-        findViewById(R.id.drawer_item_setting).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-        findViewById(R.id.drawer_item_about).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
+        mNavigationView = (NavigationView) findViewById(R.id.id_nv_menu);
+        //设置抽屉里item的监听
+        setupDrawerContent(mNavigationView);
     }
 
+    private void setupDrawerContent(NavigationView navigationView)
+    {
+        navigationView.setNavigationItemSelectedListener(
+
+                new NavigationView.OnNavigationItemSelectedListener()
+                {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem)
+                    {
+                        switch (menuItem.getItemId()) {
+                            case R.id.drawer_item_control_city:
+                                Toast.makeText(MainActivity.this,"control_city",Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.drawer_item_setting:
+                                Toast.makeText(MainActivity.this,"setting",Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.drawer_item_about:
+                                Toast.makeText(MainActivity.this,"about",Toast.LENGTH_SHORT).show();
+                                break;
+                        }
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                });
+    }
+    //菜单选项的回调
     private Toolbar.OnMenuItemClickListener onMenuItemClick = new Toolbar.OnMenuItemClickListener() {
         @Override
         public boolean onMenuItemClick(MenuItem menuItem) {
             switch (menuItem.getItemId()) {
                 case R.id.action_add_city:
-                Toast.makeText(MainActivity.this,"hhh",Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this,"添加城市",Toast.LENGTH_SHORT).show();
                     break;
             }
             return true;
@@ -113,6 +83,9 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return super.onCreateOptionsMenu(menu);
+       getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
     }
+
+
 }
